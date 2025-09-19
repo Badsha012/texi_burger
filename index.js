e2ef86686ebba1470b4febda7a3bc13f1fffc0b8
Badsha012 <badshasheikh361@gmail.com>
2025-09-19 20:17:39 +0600

    five commit

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const loadData= () =>{
+const loadCateory= () =>{
     const url="https://taxi-kitchen-api.vercel.app/api/v1/categories";
 
 
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displayCategory(data.categories));
 };
 
-loadData();
+
+const displayCategory=(categories) =>{
+    //console.log(categories);
+
+
+  
+    const  catContainer=document.getElementById("category-container");
+   // console.log(catContainer);
+    catContainer.innerHTML="";
+    for(let cat of categories){
+
+        // creae a html element
+
+     
+
+
+       const categoryCard=document.createElement("div");
+       categoryCard.innerHTML=`
+              <button class="btn btn-block justify-start shadow btn-category">
+            <img
+              src="${cat.categoryImg}"
+              alt=""
+              class="w-10"
+            />${cat.categoryName}
+          </button>`;
+
+
+        //   4 append kora
+        catContainer.append(categoryCard);
+
+    }
+
+
+}
+
+loadCateory();
