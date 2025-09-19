9e66a581a284a8698cb4c6dc424d5df4b407f1bc
Badsha012 <badshasheikh361@gmail.com>
2025-09-19 19:14:35 +0600

    nine commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const loadData= () =>{
-    const url="https://taxi-kitchen-api.vercel.app/api/v1/categories"
-}
+    const url="https://taxi-kitchen-api.vercel.app/api/v1/categories";
+
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+};
+
+loadData();
